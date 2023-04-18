60a36626437710b1ca54abd3e6b301a12bcc6ddb
Eduardo Henrique <eduardohairescampos@gmail.com>
2023-04-18 19:43:45 -0300

    feat: add_AppError

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
+import "express-async-errors";
 import express, { Application, json } from 'express'
 import { userRoutes } from './routes/users.routes'
+import { errorHandler } from './error'
 
 const app: Application = express()
 app.use(express.json())
 
 app.use('/users',userRoutes)
+
+app.use(errorHandler)
 
 export default app

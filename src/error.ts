89a51d97af2c928aed40ddc5516962860a6d7d74
Eduardo Henrique <eduardohairescampos@gmail.com>
2023-04-18 20:13:05 -0300

    feat: add_middleware_emailExists

--- src/error.ts
+++ src/error.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 
-class AppError extends Error {
+export class AppError extends Error {
   statusCode: number
   constructor(message: string, statusCode: number = 400) {
     super(message);
